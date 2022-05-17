894241522bfc5721ab5746a2ab1823f22d0478f3
Fx Tarbouriech <fxtarbouriech@hotmail.fr>
2022-05-17 15:49:10 +0200

    Added route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 
-
+User.destroy_all
 
 10.times do |index|
   user = User.create(first_name: Faker::Name.first_name, email: "email#{index}@yopmail.com", last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, encrypted_password: 'userpassword')
