dfb5da297c18d22716b050e22331dcf8ff4c8072
Fx Tarbouriech <fxtarbouriech@hotmail.fr>
2022-05-17 19:10:07 +0200

    nouveau repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'events#index'
   get 'static_pages/secret'
   devise_for :users
+  resources :events
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
